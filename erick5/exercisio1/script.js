64e654e5313f6aca02ff7806cc1451f30c4f240d
ersrck12 <santos.ramos.erick@escola.pr.gov.br>
2025-06-10 12:20:14 -0300

    Add files via upload

---
+++ erick5/exercisio1/script.js
@@ -0,0 +1,27 @@
+let num1;
+
+let num2;
+
+let resultado;
+
+let parouimpar;
+
+num1 = Number(window.prompt("digite um numero"));
+
+num2 = Number(window.prompt("digite outro numero"));
+
+resultado = num1 + num2;
+
+parouimpar = resultado % 2;
+
+if(parouimpar == 0){
+
+alert("o número + resultado + é par")
+
+}else {
+
+alert("o número + resultado + é impar")
+
+}
+
+//DESAFIO:informe na frase se o número é par ou impar.
